c272091e9dfe2e3df96d48fcdebddd509be6af16
Baaba <baabadampare@gmail.com>
2022-10-20 23:50:10 +0000

    write functions for file upload

--- src/components/FileUpload/FileUpload.js
+++ src/components/FileUpload/FileUpload.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiFillPlusCircle } from "react-icons/ai";
 
-const FileUpload = ({ files, setFiles }) => {
-  const uploadHandler = (event) => {
-    const file = event.target.files[0];
-    const newFiles = [...files];
-    newFiles.push(file);
-    setFiles(newFiles);
+const FileUpload = () => {
+  const [selectedImage, setSelectedImage] = useState();
+
+  // This function will be triggered when the file field change
+  const imageChange = (e) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setSelectedImage(e.target.files[0]);
+    }
   };
+
+  // This function will be triggered when the "Remove This Image" button is clicked
+  const removeSelectedImage = () => {
+    setSelectedImage();
+  };
+
   return (
     <>
       <div className="flex flex-col border-2 border-dashed justify-center items-center w-full p-[1.5rem]">
         <div className="flex flex-col items-center relative">
           <span className="relative text-right max-w-[10rem] z-20 cursor-pointer opacity-0">
-            <input type="file" onChange={uploadHandler}/>
+            <input accept="image/*" type="file" onChange={imageChange} />
           </span>
           <br></br>
           <button className="flex items-center justify-center gap-3 bg-bg-o text-white py-[0.4rem] px-[1rem] mb-2 absolute top-0 cursor-pointer z-10">
@@ -22,6 +30,16 @@ const FileUpload = ({ files, setFiles }) => {
             </span>
             Upload
           </button>
+
+          {selectedImage && (
+            <div>
+              <img
+                src={URL.createObjectURL(selectedImage)}
+                alt="Upload Preview"
+              />
+              <button onClick={removeSelectedImage}>Remove This Image</button>
+            </div>
+          )}
         </div>
 
         <p className="font-bold">Supported files</p>
