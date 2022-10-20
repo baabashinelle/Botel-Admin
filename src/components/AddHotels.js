c3b6b99b99f6ee6f9cc6e5ca9d564061017e9959
Baaba <baabadampare@gmail.com>
2022-10-20 22:29:26 +0000

    edit add hotels

--- src/components/AddHotels.js
+++ src/components/AddHotels.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from "react";
+import Dashboard from "../components/Dashboard";
+import FileUpload from "./FileUpload/FileUpload";
 
 const AddHotels = () => {
+  const [files, setFiles] = useState([
+    {
+      name: "myFile.pdf",
+    },
+  ]);
   return (
-    <div>AddHotels</div>
-  )
-}
+    <Dashboard>
+      <section className="flex flex-col justify-center items-center bg-white font-text px-[2rem] py-[2rem] m-[2rem] rounded-lg">
+        <h1 className="text-3xl font-bold pb-2">Add Hotels</h1>
+        <p>Please fill the form to add hotel rooms.(all fields are required)</p>
+        <article className="flex flex-col gap-[0.1rem] w-1/2 pt-10">
+          <label className="pb-2">Name of Hotel</label>
+          <input
+            type="text"
+            name="hotelName"
+            onChange={(e) => {
+              console.log(e.target.value);
+            }}
+            className="h-[3rem] px-[1rem] border-none focus:outline-bg-o bg-gray-100"
+          ></input>
+          <br></br>
+          <label className="pb-2">Country</label>
+          <input
+            type="text"
+            className="h-[3rem] px-[1rem] border-none focus:outline-bg-o bg-gray-100"
+          ></input>
+          <br></br>
+          <label className="pb-2">
+            Hotel status
+            <span className="text-gray-500 text-sm">
+              (Available / Unavailable)
+            </span>
+          </label>
+          <input
+            type="text"
+            className="h-[3rem] px-[1rem] border-none focus:outline-bg-o bg-gray-100"
+          ></input>
+          <br></br>
+          <label className="pb-2">Hotel Description</label>
+          <textarea className="min-h-[10rem] p-[1rem] border-none focus:outline-bg-o bg-gray-100"></textarea>
+          <article className="pt-10">
+            <FileUpload files={files} setFiles={setFiles} />
+          </article>
+          <button className="bg-bg-o text-white py-[0.7rem] mt-8">
+            Add Hotel
+          </button>
+        </article>
+      </section>
+    </Dashboard>
+  );
+};
 
-export default AddHotels
+export default AddHotels;
--- src/components/FileUpload/FileUpload.js
+++ src/components/FileUpload/FileUpload.js
@@ -2,13 +2,18 @@ import React from "react";
 import { AiFillPlusCircle } from "react-icons/ai";
 
 const FileUpload = ({ files, setFiles }) => {
-  const uploadHandler = () => {};
+  const uploadHandler = (event) => {
+    const file = event.target.files[0];
+    const newFiles = [...files];
+    newFiles.push(file);
+    setFiles(newFiles);
+  };
   return (
     <>
       <div className="flex flex-col border-2 border-dashed justify-center items-center w-full p-[1.5rem]">
         <div className="flex flex-col items-center relative">
           <span className="relative text-right max-w-[10rem] z-20 cursor-pointer opacity-0">
-            <input type="file"/>
+            <input type="file" onChange={uploadHandler}/>
           </span>
           <br></br>
           <button className="flex items-center justify-center gap-3 bg-bg-o text-white py-[0.4rem] px-[1rem] mb-2 absolute top-0 cursor-pointer z-10">
