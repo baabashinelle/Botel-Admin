7139984cbdf49d3661e4debebc639c9e848dd469
Baaba <baabadampare@gmail.com>
2022-10-21 17:08:48 +0000

    edit react select styles

--- src/components/StatusSelect.js
+++ src/components/StatusSelect.js
@@ -6,10 +6,26 @@ const options = [
   { value: "Unavailable", label: "Unavailable" },
 ];
 
+const customStyles = {
+  option: (provided, state) => ({
+    ...provided,
+    color: state.isSelected ? "white" : "black",
+    backgroundColor: state.isSelected ? "#fa7436" : "white",
+    cursor: "pointer",
+  }),
+  control: (provided, state) => ({
+    ...provided,
+    backgroundColor: "white",
+    cursor: "pointer",
+    outline: state.isFocused ? "2px solid #fa7436" : "none",
+    padding: "0.3rem 0.3rem",
+  }),
+};
+
 const StatusSelect = () => {
   return (
     <div>
-        <Select options={options} />
+        <Select options={options} styles={customStyles}/>
     </div>
   )
 }
