4fccca7ed87929790a3e66b8546406ea9d19c871
Bentil Shadrack <bentilshadrack72@gmail.com>
2022-10-25 16:41:49 +0000

    Create Upload image to firebase function

---
+++ firebase/index.js
@@ -0,0 +1,44 @@
+import { deleteObject, getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+
+import { storage } from "../firebase.config";
+
+export const uploadImage = async (image, storagePath, callback) => {
+  // new date to iso string remove : and . and replace with -
+  const id = new Date().toISOString().replace(/:|\./g, "-");
+  const storageRef = ref(storage, `${storagePath}/${id}-${image.name}`);
+  const uploadTask = uploadBytesResumable(storageRef, image);
+  uploadTask.on(
+    "state_changed",
+    (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log("Image Upload is " + progress + "% done");
+    },
+    (error) => {
+      switch (error.code) {
+        case "storage/unauthorized":
+          console.log("User doesn't have permission to access the object");
+          break;
+        case "storage/canceled":
+          console.log("User canceled the upload");
+          break;
+        case "storage/unknown":
+          console.log("Unknown error occurred, inspect error.serverResponse");
+          break;
+      }
+      return error;
+    },
+    () => {
+      getDownloadURL(storageRef).then((downloadURL) => {
+        console.log("File available at", downloadURL);
+        callback(downloadURL);
+      });
+    }
+  );
+};
+
+export const removeImage = async (imageURL) => {
+    const deleteRef = ref(storage, imageURL);
+    deleteObject(deleteRef).then(() => {
+      console.log("Image deleted successfully");
+    });
+}
