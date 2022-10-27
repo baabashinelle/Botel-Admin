dea7728a62281fdbae020983ac448c8438f545c7
qbentil <bentilshadrack72@gmail.com>
2022-10-27 10:21:44 +0000

    Complete fix

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,13 +2,7 @@ import axios from "axios"
 
 const Axios =  axios.create({
     baseURL: "https://ibookings-api.herokuapp.com/",
-    withCredentials: true,
-    headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-    },
-    
+    withCredentials: true,    
 })
 
 export default Axios;
