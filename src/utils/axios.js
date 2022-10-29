3abe1db75f6f5ae0f032c892b8e77ceb24fc40c9
qbentil <bentilshadrack72@gmail.com>
2022-10-29 02:26:48 +0000

    Update axios base file

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const Axios =  axios.create({
-    baseURL: "https://ibookings-api.herokuapp.com/",
+    baseURL: process.env.REACT_APP_API_BASE_URL,
     withCredentials: true,    
 })
 
